2a83447021261afd0692e96b42e15f7a13349805
GzxNydia <1375148321@qq.com>
2018-02-26 11:30:14 +0800

    完善前端

---
+++ pages/users/bookmarks.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <my-header></my-header>
+        <div class="my-container" style="padding:100px 0">
+            <img src="../../assets/img/shoucang.png" alt="" style="width: 80%;margin: 10px auto">
+        </div>
+    </div>
+</template>
+
+<script>
+  import myHeader from '../../components/myHeader'
+  export default {
+    name: 'myhomepage',
+    components:{
+      myHeader
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
